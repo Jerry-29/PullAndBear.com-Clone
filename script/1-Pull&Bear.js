80dd5cc938caa049796b645d248168d9eb150b40
CvamSinghh <91656460+CvamSinghh@users.noreply.github.com>
2021-10-26 01:34:58 +0530

    1st page

---
+++ script/1-Pull&Bear.js
@@ -0,0 +1,14 @@
+
+
+let c=document.getElementById("c")
+let p=document.getElementById("p")
+let l=document.getElementById("l")
+let bt=document.querySelector("a")
+function check(){
+if(c.value==="0" ||l.value==="0"|| p.value==="0"){
+   alert("select valid input")
+}else{
+    bt.href="2-signin.html"
+}
+}
+
