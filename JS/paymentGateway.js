5b1a620b4312a668d6ca57f78c91ee1483271dd3
Govindsingh29 <govindsingh0498@gmail.com>
2021-10-28 21:29:47 +0530

    smallbugsfixed

--- JS/paymentGateway.js
+++ JS/paymentGateway.js
@@ -1,6 +1,6 @@
 setTimeout(function(){
     window.location.href = '../HTML/11-thankyou.html';
- }, 3000);
+ }, 4000);
  var timeleft = 0;
 var downloadTimer = setInterval(function(){
 document.getElementById("timer").innerHTML = 3 - timeleft;
